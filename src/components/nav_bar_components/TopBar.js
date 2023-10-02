aa470e2b2e9b26a2293c1090b9808e2d9ae82f10
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2023-10-03 01:27:03 +0500

    navlink route use heigh light the foute tab

--- src/components/nav_bar_components/TopBar.js
+++ src/components/nav_bar_components/TopBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function TopBar() {
     return (
@@ -8,10 +8,22 @@ function TopBar() {
                 {/* <li><a href="/"> Home</a></li>
                 <li><a href="/about">About </a></li>
                 <li><a href="/get-id/123">Id in url</a></li> */}
-                <Link to="/"> Home</Link>
+                {/* <Link to="/"> Home</Link>
                 <Link to="/about"> | About</Link>
                 <Link to="/get-id/123"> | Id in url </Link>
-                <Link to="/state-data" state={{ name: 'emmaneul', father: 'saleem' }}> | also pass data through state in link </Link>
+                <Link to="/state-data" state={{ name: 'emmaneul', father: 'saleem' }}> | also pass data through state in link </Link> */}
+                <NavLink style={({ isActive }) => {
+                    return { color: isActive ? 'red' : '' }
+                }} to="/"> Home</NavLink>
+                <NavLink style={({ isActive }) => {
+                    return { color: isActive ? 'red' : '' }
+                }} to="/about"> | About</NavLink>
+                <NavLink style={({ isActive }) => {
+                    return { color: isActive ? 'red' : '' }
+                }} to="/get-id/123"> | Id in url </NavLink>
+                <NavLink style={({ isActive }) => {
+                    return { color: isActive ? 'red' : '' }
+                }} to="/state-data" state={{ name: 'emmaneul', father: 'saleem' }}> | also pass data through state in link </NavLink>
             </ul>
         </div>
     )
